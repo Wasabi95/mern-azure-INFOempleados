13c1e66a85c671313e07015c1278474a38644c1c
Wasabi95 <ancarsa.wasabi.12@gmail.com>
2024-08-28 06:55:49 -0500

    Hellox

--- client/src/baseUrl.js
+++ client/src/baseUrl.js
@@ -6,6 +6,6 @@
 // const baseUrl = 'http://localhost:3001'; // Ensure this is the correct base URL
 // export default baseUrl;
 
-const baseUrl = "http://localhost:3001";
+const baseUrl = "http://mern-azure-app1-bebkh3dgggaegng6.eastus-01.azurewebsites.net";
 
 export default baseUrl;
